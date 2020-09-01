9e87b8cfbc8624cf2b2cb11b4e9f2b4465e93c8d
sandsor <70569977+sandsor@users.noreply.github.com>
2020-09-01 11:51:45 +0200

    added some comments and wrote a little more

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -1,8 +1,15 @@
-
 #include <iostream>
+/*
+Comment Spot
+*/
+int age;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    std::cout << " Goodbye World";
+int main() {
+
+	// text based code
+	std::cout << "Enter your age :"; // enter your age
+	std::cin >> age; // allows player to print age
+	std::cout << "\t Your age is : " << age << "\n"; // "\n" = std::endl;   \t     // can use /* */ to comment out
+	std::cout << "You are young " << std::endl;
+
 }
