d261294b5b3a1350b11dc6d16b5eda6ecb42712c
sandsor <70569977+sandsor@users.noreply.github.com>
2020-09-01 12:31:02 +0200

    tried experimenting with if's

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -3,12 +3,11 @@
 /*
 Comment Spot
 */
-int age;
+int age, agee;
 bool sw = true; // true = 1 || false = 0  bool is true or false
 char me = 'm';
 std::string name = "sander";
 double pi = 3.14; 
-
 
 int main() {
 
@@ -16,6 +15,14 @@ int main() {
 	std::cout << "Enter your age :"; // enter your age
 	std::cin >> age; // allows player to print age
 	std::cout << "\t Your age is : " << age << "\n"; // "\n" = std::endl;   \t     // can use /* */ to comment out
-	std::cout << "You are young " << std::endl;
-
+	std::cout << "\t You are young " << std::endl;
+	std::cout << "How old are you next year?" << std::endl;
+	std::cin >> agee;
+	if (agee < 20)
+		std::cout << "That's too Young!" << std::endl;
+	if (agee > 20)
+		std::cout << "Good :D" << std::endl;
+	if (agee == 20)
+		std::cout << "Nice, me too" << std::endl;
+	return 0;
 }
