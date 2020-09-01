0412bb97eb1763667a301b4f4289d3b96d103431
sandsor <70569977+sandsor@users.noreply.github.com>
2020-09-01 13:26:59 +0200

    can now write name

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -6,22 +6,29 @@ Comment Spot
 int age, agee;
 bool sw = true; // true = 1 || false = 0  bool is true or false
 char me = 'm';
-std::string name = "sander";
+std::string Fullname = "";
 double pi = 3.14; 
 
 int main() {
 
 	// text based code
+	std::cout << "enter your full name : " ;
+	std::getline(std::cin, Fullname);
+	std::cout << "your name is : " << Fullname << std::endl;
+
 	std::cout << "Enter your age :"; // enter your age
 	std::cin >> age; // allows player to print age
 	std::cout << "\t Your age is : " << age << "\n"; // "\n" = std::endl;   \t     // can use /* */ to comment out
-	std::cout << "\t You are young " << std::endl;
+	std::cout << "\t That's cool " << std::endl;
+	
+	
 	std::cout << "How old are you next year?" << std::endl;
+	// here i wanted to add a variable, so that what you write impacts the game
 	std::cin >> agee;
 	if (agee < 20)
 		std::cout << "That's too Young!" << std::endl;
 	if (agee > 20)
-		std::cout << "Good :D" << std::endl;
+		std::cout << "Then you are older than me :D" << std::endl;
 	if (agee == 20)
 		std::cout << "Nice, me too" << std::endl;
 	return 0;
